116c2ac5c0e5364f3e405fc20941d1b7dcfcc9a8
alakijaayo <alakijaayo@gmail.com>
2019-05-09 17:06:18 +0100

    Working Raindrops

--- src/Raindrops.java
+++ src/Raindrops.java
@@ -1,2 +1,28 @@
 public class Raindrops {
+
+    public static String numbers(int choice) {
+        String output = "";
+
+        if (choice % 3 == 0) {
+            output += "Pling";
+        }
+        if (choice % 5 == 0) {
+            output += "Plang";
+        }
+        if (choice % 7 == 0) {
+            output += "Plong";
+        }
+        if (output.length() == 0) {
+            output += choice;
+        }
+
+        return output;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(numbers(3));
+        System.out.println(numbers(8));
+        System.out.println(numbers(300));
+    }
+
 }
